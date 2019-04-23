e2a2ade62ab02d15101269746ed16d9c3b2c7c7d
github username <github email address>
2019-04-23 18:11:27 -0400

    Done.

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -3,23 +3,38 @@ import React from "react";
 class LoginForm extends React.Component {
   constructor() {
     super();
+    this.state = {
+      username: '',
+      password: ''
+    };
+  }
 
-    this.state = {};
+  handleChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+
+  login = (e) => {
+    e.preventDefault()
+    if(this.state.username && this.state.password){
+      this.props.handleLogin(e)
+    }
   }
 
   render() {
     return (
-      <form>
+      <form onSubmit={(e)=>this.login(e)}>
         <div>
           <label>
             Username
-            <input id="username" name="username" type="text" />
+            <input id="username" name="username" type="text" onChange={(e) => this.handleChange(e)} value={this.state.username}/>
           </label>
         </div>
         <div>
           <label>
             Password
-            <input id="password" name="password" type="password" />
+            <input id="password" name="password" type="password" onChange={(e) => this.handleChange(e)} value={this.state.password}/>
           </label>
         </div>
         <div>
--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -1,17 +1,27 @@
 import React from "react";
 
 class TwitterMessage extends React.Component {
-  constructor() {
-    super();
 
-    this.state = {};
+  constructor(props){
+    super(props)
+    this.state = {
+      tweetMessage: '',
+    }
   }
+
+  writeTweet = (e) => {
+    this.setState({
+      tweetMessage: e.target.value,
+    })
+  }
+
 
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" name="message" id="message" />
+        <input type="text" name="message" id="message" onChange={(e) => this.writeTweet(e)} value={this.state.tweetMessage}/>
+        <p>{this.props.maxChars - this.state.tweetMessage.length}</p>
       </div>
     );
   }
